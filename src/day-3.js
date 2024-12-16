8e20a6823dd7466e112c0aee12790fc79625fee3
Harry Fitzgerald <harry.fitzgerald@globalfreightsolutions.com>
2024-12-16 14:44:04 +0000

    day3-all

--- src/day-3.js
+++ src/day-3.js
@@ -1,10 +1,17 @@
 import { getInput } from "./util.js"
 
+
+// warning: here be regex
+
+async function getLines() {
+    const input = await getInput(3, 1)
+    return input.join('\n')
+}
+
 export async function part1() {
-    const lines = await getInput(3, 1)
+    const lines = await getLines()
 
-
-    console.log([...lines.join('\n').matchAll(/mul\([0-9]+,[0-9]+\)/g)]
+    console.log([...lines.matchAll(/mul\([0-9]+,[0-9]+\)/g)]
 	.map(mt => mt[0])
 	.map(
 	    sum => sum
@@ -15,7 +22,31 @@ export async function part1() {
 	)
 	.reduce((a, c) => a + c, 0)
     )
+}
 
+export async function part2() {
+    const lines = await getLines()
 
+    const matches = [...lines.matchAll(/(do\(\)|don\'t\(\)|mul\([0-9]+,[0-9]+\))/g)].map(mt => mt[0])
+
+    let total = 0
+    let shouldDo = true
+    for (const line of matches) {
+	if (line === 'do()') {
+	    shouldDo = true
+	} else if (line === "don't()") {
+	    shouldDo = false
+	} else {
+	    if (shouldDo) {
+		total += line
+		    .replace(/[^0-9,]/g, '')
+		    .split(',')
+		    .map(Number)
+		    .reduce((a, c) => a * c, 1)
+	    }
+	}
+    }
+
+    console.log(total)
 }
 
